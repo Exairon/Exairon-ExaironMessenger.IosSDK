e092b2a79419512db824cd7544a63b470fa34b9a
Enes Toprak <etoprak174@gmail.com>
2023-02-06 16:19:19 +0300

    add splash page icon

--- Framework/ExaironFramework/ExaironFramework/Views/SplashView.swift
+++ Framework/ExaironFramework/ExaironFramework/Views/SplashView.swift
@@ -9,10 +9,28 @@ import SwiftUI
 
 struct SplashView: View {
     @ObservedObject var chatViewModel: ChatViewModel
+    @State private var scale = 1.0
+    @State private var scaleSize = 0.05
+    @State private var scaleUp = true
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+
     
     var body: some View {
-        ProgressView().onAppear{
-            chatViewModel.getWidgetSettings(){widgetSettings in }
-        }
+        Image("exa_logo")
+            .resizable()
+            .frame(width: 150, height: 150)
+            .scaleEffect(scale)
+            .onReceive(timer) { input in
+                scale += scaleSize
+                print(scale)
+                if Double(round(100 * scale) / 100) == 2.0 {
+                    scaleSize = -0.05
+                } else if Double(round(100 * scale) / 100) == 1.0  {
+                    scaleSize = 0.05
+                }
+            }
+            .onAppear{
+                chatViewModel.getWidgetSettings(){widgetSettings in }
+            }
     }
 }
