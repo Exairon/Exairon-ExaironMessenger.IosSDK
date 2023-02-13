8e410c324bf5d78f1a60693c285a85b30f4e66c7
Enes Toprak <etoprak174@gmail.com>
2023-02-13 15:20:37 +0300

    fix text message view

--- Framework/ExaironFramework/ExaironFramework/Components/Messages/TextMessageView.swift
+++ Framework/ExaironFramework/ExaironFramework/Components/Messages/TextMessageView.swift
@@ -6,6 +6,19 @@
 //
 
 import SwiftUI
+import WebKit
+
+struct HTMLStringView: UIViewRepresentable {
+    let htmlContent: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.loadHTMLString(htmlContent, baseURL: nil)
+    }
+}
 
 struct TextMessageView: View {
     @State var message: Message
@@ -15,7 +28,7 @@ struct TextMessageView: View {
         if message.sender.contains("user_uttered") {
             HStack {
                 Spacer()
-                Text(message.text ?? "")
+                Text(htmlStringToSwiftString(text: message.text?.replacingOccurrences(of: "&lt;", with: "<") ?? ""))
                     .padding()
                     .foregroundColor(Color(hex: widgetSettings.data.color.userMessageFontColor))
                     .background(Color(hex: widgetSettings.data.color.userMessageBackColor))
@@ -24,7 +37,7 @@ struct TextMessageView: View {
             }
         } else {
             HStack {
-                Text(message.text ?? "")
+                Text(htmlStringToSwiftString(text: message.text?.replacingOccurrences(of: "&lt;", with: "<") ?? ""))
                     .padding()
                     .foregroundColor(Color(hex: widgetSettings.data.color.botMessageFontColor))
                     .background(Color(hex: widgetSettings.data.color.botMessageBackColor))
@@ -33,5 +46,20 @@ struct TextMessageView: View {
                 Spacer()
             }
         }
+        /*Text("Regular")
+        Text("*Italics*")
+        Text("**Bold**")
+        Text("~Strikethrough~")
+        Text("`Code`")
+        Text("[Link](https://apple.com)")
+        Text("***[They](https://apple.com) ~are~ `combinable`***")*/
+        
+    }
+    
+    func htmlStringToSwiftString(text: String) -> String {
+        //var swiftString = text.replacingOccurrences(of: "<i>", with: "*").replacingOccurrences(of: "</i>", with: "*")
+        //swiftString = swiftString.replacingOccurrences(of: "<strong>", with: "**").replacingOccurrences(of: "</strong>", with: "**")
+        //swiftString = swiftString.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
+        return text.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
     }
 }
