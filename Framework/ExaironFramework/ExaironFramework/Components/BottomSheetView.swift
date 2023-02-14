125addcbfe5d0f10b7e2a3a5815054795e2bf8f8
Enes Toprak <etoprak174@gmail.com>
2023-02-14 23:08:17 +0300

    add location and document picker

--- Framework/ExaironFramework/ExaironFramework/Components/BottomSheetView.swift
+++ Framework/ExaironFramework/ExaironFramework/Components/BottomSheetView.swift
@@ -5,20 +5,30 @@
 //  Created by Exairon on 14.02.2023.
 //
 
+import Foundation
 import SwiftUI
 import PhotosUI
+import UIKit
+import MapKit
+
+enum MenuElement {
+    case camera
+    case gallery
+    case file
+    case location
+}
 
 struct BottomSheetView: View {
     var body: some View {
         HStack {
             VStack {
-                BottomSheetElementView(text: "camera", icon: "camera")
+                BottomSheetElementView(element: .camera, icon: "camera")
                 Divider()
-                BottomSheetElementView(text: "gallery", icon: "photo")
+                BottomSheetElementView(element: .gallery, icon: "photo")
                 Divider()
-                BottomSheetElementView(text: "file", icon: "doc")
+                BottomSheetElementView(element: .file, icon: "doc")
                 Divider()
-                BottomSheetElementView(text: "location", icon: "location")
+                BottomSheetElementView(element: .location, icon: "location")
             }
             /*VStack(alignment:.trailing) {
                 HStack {
@@ -39,20 +49,33 @@ struct BottomSheetView: View {
 }
 
 struct BottomSheetElementView: View {
+    //Gallery
     @State private var selectedItem: PhotosPickerItem? = nil
     @State private var selectedImageData: Data? = nil
+    //Document
+    @State var showDocumentSheet = false
+    //Location
+    @State var showMapSheet = false
+    @StateObject private var mapViewModel = MapViewModel()
     
-    var text: String
+    var element: MenuElement
     var icon: String
+    
     var body: some View {
         VStack {
-            switch(text) {
-            case "gallery":
+            switch(element) {
+            case .camera:
+                Button {
+                    print("click")
+                } label: {
+                    IconButtonView(text: "camera", icon: icon)
+                }
+            case .gallery:
                 PhotosPicker(
                     selection: $selectedItem,
                     matching: .images,
                     photoLibrary: .shared()) {
-                        IconButtonView(text: text, icon: icon)
+                        IconButtonView(text: "gallery", icon: icon)
                     }
                     .onChange(of: selectedItem) { newItem in
                         Task {
@@ -62,11 +85,50 @@ struct BottomSheetElementView: View {
                             }
                         }
                     }
-            default:
+            case .file:
                 Button {
-                    print(text)
+                    self.showDocumentSheet.toggle()
                 } label: {
-                    IconButtonView(text: text, icon: icon)
+                    IconButtonView(text: "file", icon: icon)
+                }
+                .sheet(isPresented: $showDocumentSheet) {
+                    DocumentPicker()
+                }
+            case .location:
+                Button {
+                    self.showMapSheet.toggle()
+                } label: {
+                    IconButtonView(text: "location", icon: icon)
+                }
+                .sheet(isPresented: $showMapSheet) {
+                    ZStack {
+                        Map(coordinateRegion: $mapViewModel.region, showsUserLocation: true)
+                            .ignoresSafeArea()
+                            .presentationDetents([.height(UIScreen.main.bounds.height * 0.7)])
+                            .onAppear {
+                                mapViewModel.checkIfLocationServicesIsEnabled()
+                            }
+                        VStack {
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                Button {
+                                    print(mapViewModel.region)
+                                } label: {
+                                    Image(systemName: "arrow.up.circle.fill").font(.system(size: 40))
+                                }
+                                    .font(.system(size: 26))
+                                    .padding(.horizontal, 10)
+                                .padding()
+                                .background(.black.opacity(0.75))
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .clipShape(Circle())
+                                .padding(.trailing)
+                            }
+                        }
+                    }
+                    
                 }
             }
         }
@@ -93,5 +155,34 @@ struct IconButtonView: View {
             maxHeight: .infinity,
             alignment: .topLeading
         )
+    }
+}
+
+struct DocumentPicker: UIViewControllerRepresentable {
+    func makeCoordinator() -> Coordinator {
+        return DocumentPicker.Coordinator(parent1: self)
+    }
+    
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<DocumentPicker>) -> UIDocumentPickerViewController {
+        let picker = UIDocumentPickerViewController(forOpeningContentTypes: [.pdf])
+        picker.allowsMultipleSelection = false
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: DocumentPicker.UIViewControllerType, context: UIViewControllerRepresentableContext<DocumentPicker>) {
+    }
+    
+    class Coordinator: NSObject, UIDocumentPickerDelegate {
+        
+        var parent: DocumentPicker
+        
+        init(parent1: DocumentPicker) {
+            parent = parent1
+        }
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            print(urls.first?.deletingPathExtension().lastPathComponent ?? "")
+        }
     }
 }
